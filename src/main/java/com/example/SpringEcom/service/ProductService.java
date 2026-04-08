6eb3b4de675c53edc7074e9476980fed7d388845
pola <polasameh9@gmail.com>
2026-04-08 13:51:19 +0200

    Project Using Spring Boot MVC

---
+++ src/main/java/com/example/SpringEcom/service/ProductService.java
@@ -0,0 +1,40 @@
+package com.example.SpringEcom.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.SpringEcom.model.Product;
+import com.example.SpringEcom.repo.ProductRepo;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepo productRepo;
+
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return productRepo.findById(id).orElse(new Product(-1));
+    }
+
+    public Product addOrUpdateProduct(Product product, MultipartFile image) throws IOException {
+        product.setImageType(image.getOriginalFilename());
+        product.setImageName(image.getName());
+        product.setImageData(image.getBytes());
+
+        return productRepo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        productRepo.deleteById(id);
+    }
+
+
+}
